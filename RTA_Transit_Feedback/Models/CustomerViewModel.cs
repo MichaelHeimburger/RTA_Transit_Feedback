56465244d8426107262cea2da57d62fbb8cb1e2d
MonicaDJohnson <johnsondmonica290@gmail.com>
2018-08-02 14:23:33 -0400

    added view models Monica PC

---
+++ RTA_Transit_Feedback/Models/CustomerViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RTA_Transit_Feedback
+{
+    [MetadataType(typeof(CustomersmetaData))]
+
+    public partial class Customers
+    {
+
+    }
+
+
+
+    public class CustomersmetaData
+    {
+        public int CustomerID { get; set; }
+        [DisplayName("First Name")]
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string AddressL1 { get; set; }
+        public string AddressL2 { get; set; }
+        public string City { get; set; }
+        public Nullable<int> stateID { get; set; }
+        public string PhoneNum { get; set; }
+        public string Id { get; set; }
+        public string Zip { get; set; }
+
+    }
+}
---
+++ RTA_Transit_Feedback/Models/FeedBackForm.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RTA_Transit_Feedback
+{
+    [MetadataType(typeof(FeedBackFormsmetaData))]//must match view folder, to update to page
+
+    public partial class FeedBackForm
+    {
+
+    }
+
+    public class FeedBackFormsmetaData
+    {
+        public int FeedbackID { get; set; }
+        [DisplayName("Feedback Description")]
+        public string FeedbackDescription { get; set; }
+        [DisplayName("Date of Ride")]
+        public Nullable<System.DateTime> DateofRide { get; set; }
+        [DisplayName("Time of Ride")]
+        public Nullable<System.TimeSpan> TimeofRide { get; set; }
+        [DisplayName("Route Name")]
+        public string RouteName { get; set; }
+        [DisplayName("Vehicle Number")]
+        public string VehNum { get; set; }
+        public Nullable<int> CustomerID { get; set; }
+        public Nullable<int> BatchID { get; set; }
+
+        public virtual Batch Batch { get; set; }
+        public virtual Customers Customers { get; set; }
+    }
+}
