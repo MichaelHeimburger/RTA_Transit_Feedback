902ab5433daf69a961cec716f7017fa9d5944cde
MonicaDJohnson <johnsondmonica290@gmail.com>
2018-08-03 10:10:31 -0400

    end of Sprint 1 push

---
+++ RTA_Transit_Feedback/Models/StateViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RTA_Transit_Feedback
+{
+    [MetadataType(typeof(statemetaData))]
+
+    public partial class state
+    {
+
+    }
+
+
+
+    public class statemetaData
+    {
+        [Required]
+        [DisplayName("State")]
+        public int stateID { get; set; }
+        [DisplayName("State")]
+        public string stateCode { get; set; }
+        [DisplayName("State")]
+        public string stateName { get; set; }
+    }
+}
