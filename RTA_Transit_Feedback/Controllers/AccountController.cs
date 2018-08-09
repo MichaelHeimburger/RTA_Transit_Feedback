03abde2d001e5c32058ffeae5431ebc5c45f8420
MichaelHeimburger <mrheimburger45@gmail.com>
2018-08-09 14:48:49 -0400

    minor styleing changes

--- RTA_Transit_Feedback/Controllers/AccountController.cs
+++ RTA_Transit_Feedback/Controllers/AccountController.cs
@@ -155,6 +155,7 @@ namespace RTA_Transit_Feedback.Controllers
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
+                   
                     await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
                     
                     // For more information on how to enable account confirmation and password reset please visit https://go.microsoft.com/fwlink/?LinkID=320771
