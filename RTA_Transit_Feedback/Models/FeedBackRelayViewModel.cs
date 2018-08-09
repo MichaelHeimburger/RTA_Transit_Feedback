85dc34c910d6ff8a4aac9946fa949b77c5eaa515
r0bulous <rrwinn@gmail.com>
2018-08-09 17:50:33 -0400

    added validation and watermark text to feedback create view

--- RTA_Transit_Feedback/Models/FeedBackRelayViewModel.cs
+++ RTA_Transit_Feedback/Models/FeedBackRelayViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,7 +10,7 @@ namespace RTA_Transit_Feedback.Models
     {
         public List<FeedBackForm> Forms { get; set; }
         public bool BatchAll { get; set; }
-
+        public bool HasDanger { get; set; }
 
     }
 }
